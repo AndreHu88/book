2c2f5d08c64e906665e586ea86a43ab52f09a5d4
jack <229376483@qq.com>
2021-11-29 18:57:42 +0800

    some update

--- docs/upload.sh
+++ docs/upload.sh
@@ -1,6 +1,10 @@
 gitbook install
 
-gitbook build ./ docs   
+gitbook build ./ docs  
+
+cp -r _book/* ../book_upload_dir/
+
+cd ../book_upload_dir/
 
 git add --all
 
@@ -8,4 +12,4 @@ git commit -m "some update"
 
 git push -u origin master
 
-osascript -e 'display notification "上传完成" with title "gitbook上传完成" subtitle "操作完成，可以体验" sound name "Glass"'
+osascript -e 'display notification "上传完成" with title "上传完成" subtitle "" sound name "Glass"'
